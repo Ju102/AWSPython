ceaf688c31bcacc17c8ce13eca40f2fcc30b89b9
Ju102 <jlordonez02@gmail.com>
2026-05-29 11:46:57 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+#Se crea un Dataframe que es el objeto que almacena datos en pandas
+df = pd.read_csv('data/got.csv')
+print(df.head(10)) #Se muestran las primeras 10 filas del Dataframe
+
+
